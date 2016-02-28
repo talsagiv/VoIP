6b89a20565524367b0bcc3077a832c91ce453551
osh <osh@osh.com>
2016-02-28 14:33:57 +0200

    alalalala

---
+++ VoIP Python/SIP.py
@@ -0,0 +1,33 @@
+class user:
+
+    def __init__(self, addr, name, online, password):
+        self.addr = addr
+        self.name= name
+        self.online
+
+
+
+def register():
+
+
+
+def invite():
+
+
+
+def ack():
+
+
+
+def bye():
+
+
+
+def cancel():
+
+
+
+def options():
+
+
+
